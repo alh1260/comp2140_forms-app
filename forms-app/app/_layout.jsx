9721649fff21ac1aa9444102c4bbcd3b1ae42f13
Al Holliday <alh1260@gmail.com>
2025-10-15 15:13:44 +1000

    Added icons to the drawer nav

--- forms-app/app/_layout.jsx
+++ forms-app/app/_layout.jsx
@@ -1,13 +1,19 @@
 import {Drawer} from "expo-router/drawer";
-import {PaperProvider} from "react-native-paper";
+import {PaperProvider, Icon} from "react-native-paper";
 
 function RootLayout()
 {
     return (
         <PaperProvider>
             <Drawer>
-                <Drawer.Screen name="index" options={{title: "Home"}}/>
-                <Drawer.Screen name="about" options={{title: "About"}}/>
+                <Drawer.Screen name="index" options={{
+                     title: "Home",
+                     drawerIcon: (opts) => (<Icon source="home" color={opts.color} size={opts.size} />)
+                 }}/>
+                <Drawer.Screen name="about" options={{
+                     title: "About",
+                     drawerIcon: (opts) => (<Icon source="information-outline" color={opts.color} size={opts.size} />)
+                 }}/>
             </Drawer>
         </PaperProvider>
     );
